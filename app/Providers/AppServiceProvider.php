8c27bd835e2c74631715024873ea45d4a97e6ad2
beshar jeiroudi <lastking711@protonmail.com>
2026-01-01 15:24:19 +0300

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -52,6 +54,20 @@ class AppServiceProvider extends ServiceProvider
         //             'width' => 300,
         //         ]
         //     );
+
+        Gate::define('viewPulse', function (?User $user = null) {
+            // Replace '192.0.2.1' with your actual public IP address
+            // $allowedIp = '192.0.2.1';
+
+            // // Get the current request
+            // $request = app(Request::class);
+
+            // // Check if the client IP matches the allowed IP
+            // return $request->ip() === $allowedIp;
+
+            return true;
+
+        });
 
     }
 }
