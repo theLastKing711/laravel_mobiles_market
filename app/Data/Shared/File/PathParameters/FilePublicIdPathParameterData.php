f1b526bfc4cbe169c3ac951ba48e9773b1903d05
beshar jeiroudi <lastking711@protonmail.com>
2026-01-31 20:44:09 +0300

    'a'

--- app/Data/Shared/File/PathParameters/FilePublicIdPathParameterData.php
+++ app/Data/Shared/File/PathParameters/FilePublicIdPathParameterData.php
@@ -3,6 +3,7 @@
 namespace App\Data\Shared\File\PathParameters;
 
 use App\Models\TemporaryUploadedImages;
+use Error;
 use OpenApi\Attributes as OAT;
 use Spatie\LaravelData\Attributes\FromRouteParameter;
 use Spatie\LaravelData\Attributes\Validation\Exists;
@@ -27,6 +28,27 @@ class FilePublicIdPathParameterData extends Data
         ]
         public string $public_id,
     ) {}
+
+    // Transform data before validation and casting
+    public static function prepareForPipeline(array $payload): array
+    {
+
+        $request_public_id =
+                $payload['public_id'];
+
+        if (! isset($request_public_id)) {
+            throw new Error('error');
+        }
+
+        $request_parsed_public_id =
+             str_replace(
+                 '-',
+                 '/',
+                 $request_public_id
+             );
+
+        return $request_parsed_public_id;
+    }
 
     // Exists(TemporaryUploadedImages::class, 'public_id')
 
