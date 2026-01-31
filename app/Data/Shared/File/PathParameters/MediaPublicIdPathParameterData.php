7df11e57fcc268cabfbf94c346d9dc592ee860a1
beshar jeiroudi <lastking711@protonmail.com>
2026-01-31 21:56:27 +0300

    'a'

--- app/Data/Shared/File/PathParameters/MediaPublicIdPathParameterData.php
+++ app/Data/Shared/File/PathParameters/MediaPublicIdPathParameterData.php
@@ -2,10 +2,8 @@
 
 namespace App\Data\Shared\File\PathParameters;
 
-use App\Models\Media;
 use OpenApi\Attributes as OAT;
 use Spatie\LaravelData\Attributes\FromRouteParameter;
-use Spatie\LaravelData\Attributes\Validation\Exists;
 use Spatie\LaravelData\Data;
 use Spatie\TypeScriptTransformer\Attributes\TypeScript;
 
@@ -23,8 +21,26 @@ class MediaPublicIdPathParameterData extends Data
                 ),
             ),
             FromRouteParameter('public_id'),
-            Exists(Media::class, 'public_id')
         ]
         public string $public_id,
     ) {}
+
+    // Transform data before validation and casting
+    public static function prepareForPipeline(array $payload): array
+    {
+
+        $request_public_id =
+                request()
+                    ->route(
+                        'public_id'
+                    );
+
+        $payload['public_id'] =
+             getUrlToDbPublicIdFormat(
+                 $request_public_id
+             );
+
+        return $payload;
+
+    }
 }
