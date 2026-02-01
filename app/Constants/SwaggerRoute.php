dc7dfeb49d93aa163f770926af1e89a09e3609c0
beshar jeiroudi <lastking711@protonmail.com>
2026-02-01 15:40:23 +0300

    'a'

--- app/Constants/SwaggerRoute.php
+++ app/Constants/SwaggerRoute.php
@@ -14,10 +14,14 @@ class SwaggerRoute
         ],
     ];
 
+    public const MAIN_ROUTES = [
+        'files' => '/users/my-mobile-offers/files',
+    ];
+
     public const MY_MOBILE_OFFER_FILE = [
         'MAIN_ROUTE' => '/users/my-mobile-offers/files',
         'CHILD_ROUTES' => [
-            'cloudinary-presigned-urls' => '/cloudinary-presigned-urls',
+            'cloudinary-presigned-urls' => self::MY_MOBILE_OFFER_FILE['CHILD_ROUTES'].'/cloudinary-presigned-urls',
             'cloudinary-notifications-url' => '/cloudinary-notifications-url',
             'media' => '/medias/{public_id}',
             'temporary-uploaded-images' => '/temporary-uploaded-images/{public_id}',
