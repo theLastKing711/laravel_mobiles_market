e5bf9f40fd668227e24e76f0d52dbcb8220fd7f3
lastking711 <lastking711@protnmail.com>
2026-05-11 20:04:29 +0300

    'a'

--- app/Http/Controllers/User/MobileOffer/SearchMobilesOffersController.php
+++ app/Http/Controllers/User/MobileOffer/SearchMobilesOffersController.php
@@ -29,7 +29,6 @@ class SearchMobilesOffersController extends Controller
 
         $is_users_logged_in =
             Auth::check();
-
         if(! $is_users_logged_in) {
 
             $remote_mobiles_offers_search =
